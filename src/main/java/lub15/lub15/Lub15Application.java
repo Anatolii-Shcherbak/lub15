794f1705dcc78ad3a46c70ec95d4182261eb9f9f
Anatolii Shcherbak 63570 <ashcher1@stu.vistula.edu.pl>
2023-12-20 21:46:52 +0100

    Initial commit

---
+++ src/main/java/lub15/lub15/Lub15Application.java
@@ -0,0 +1,13 @@
+package lub15.lub15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lub15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lub15Application.class, args);
+    }
+
+}
---
+++ src/test/java/lub15/lub15/Lub15ApplicationTests.java
@@ -0,0 +1,13 @@
+package lub15.lub15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lub15ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
